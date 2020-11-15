c346e97a9a5147f7f298ea0820c29ebaba8836cd
sebastians9517 <sebastians9517@gmail.com>
2020-11-15 12:23:05 -0600

    winnercheck function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 const colors = {
-    '': 'empty',
+    '0': 'empty',
      '1': 'player1',
      '-1': 'player2'
 };
@@ -25,14 +25,14 @@ let winner;
 
 /*------------------------ Cached Element References ------------------------*/
 // You might choose to put your game status here
-let announcement = document.getElementById('message')
-let spaces =  Array.from(document.querySelectorAll('#board div'));
+let squares =  Array.from(document.querySelectorAll('#board div'));
+let messages = document.getElementById('message');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 // This is where you should put the event listener
 // for a mouse-click
-document.getElementById('board').addEventListener('click', play);
+document.getElementById('board').addEventListener('click', handleTurn);
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -44,39 +44,79 @@ document.getElementById('board').addEventListener('click', play);
 // what the board will look like upon loading
 
 function init() {
-board = ['', '', '',
-         '', '', '',
-         '', '', ''];
-
-    render();
+    board = ['', '', '',
+             '', '', '',
+             '', '', '' ];
+        render();
 };
-init();
+    init();
 
 // On-Click function:
 // Set up what happens when one of the elements
 // is clicked
-function play(event) {
-    let index = spaces.findIndex((space) => {
-        return spaces === event.target;
+function handleTurn(event) {
+    let idx = squares.findIndex((square) => {
+    return square === event.target;
     });
-    board[index] = turn;
-    console.log(board)
+    board[idx] = turn;
+    console.log(board);
+
+    if (turn === 'X') {
+        turn = 'O'
+    } else {
+        turn = 'X'};
+    winnerCheck();
+    render();
 };
+
 
 // Check winner function:
 // Checks the current state of the board for
 // a winner and changes the state of the winner
 // variable if so
 
+function winnerCheck() {
+    for(let i = 0; i < winningCombinations.length; i++){
+    if (board[0] == board[1] && board[1] == board[2])
+        return 1;
+    else if (board[3] == board[4] && board[4] == board[5])
+        return 1;
+    else if (board[6] == board[7] && board[7] == board[8])
+        return 1;
+    else if (board[0] == board[3] && board[3] == board[6])
+        return 1;
+    else if (board[1] == board[4] && board[4] == board[7])
+        return 1;
+    else if (board[2] == board[5] && board[5] == board[8])
+        return 1;
+    else if (board[0] == board[4] && board[4] == board[8])
+        return 1;
+    else if (board[2] == board[4] && board[4] == board[6])
+        return 1;
+    else if ((board[1] != 'X' || board[1] == 'O') &&
+             (board[2] != 'X' || board[2] == 'O') &&
+             (board[3] != 'X' || board[3] == 'O') &&
+             (board[4] != 'X' || board[4] == 'O') &&
+             (board[5] != 'X' || board[5] == 'O') &&
+             (board[6] != 'X' || board[6] == 'O') &&
+             (board[7] != 'X' || board[7] == 'O') &&
+             (board[8] != 'X' || board[8] == 'O') &&
+             (board[9] != 'X' || board[9] == 'O')) {
+             console.log('hi')
+        return 0;}
+
+    else
+        return -1;
+}}
+
 
 // Render function:
 // Displays the current state of the board
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
-
 function render() {
     board.forEach((mark, index) => {
-        console.log(mark, index)
-        spaces[index].textContent = turn;
+    squares[index].textContent = mark;
     });
+    messages.textContent = `Time for ${turn} to play`;
 };
