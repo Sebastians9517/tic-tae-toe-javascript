5426b6c43b4e8fdc6905201349bd6cd6bd4e3e7f
sebastians9517 <sebastians9517@gmail.com>
2020-11-15 22:53:03 -0600

    include music and aditional styling

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
-/*-------------------------------- Constants --------------------------------*/
 
 const winningCombinations = [
                 ['0', '1', '2'], ['0', '4', '8'], ['0', '3', '6'], ['3', '4', '5'],
                 ['6', '7', '8'], ['1', '4', '7'], ['2', '5', '8'], ['2', '4', '6']
 ];
 
-/*---------------------------- Variables (state) ----------------------------*/
-
-// Variables might include (board/turn/winner)
 let board;
-let turn = 'X';
+let turn = '🗿';
 let win;
 
-
-/*------------------------ Cached Element References ------------------------*/
-// You might choose to put your game status here
 let squares =  Array.from(document.querySelectorAll('#board div'));
 let messages = document.querySelector('h2');
 
-/*----------------------------- Event Listeners -----------------------------*/
 
-// This is where you should put the event listener
-// for a mouse-click
 document.getElementById('board').addEventListener('click', handleTurn);
 document.getElementById('restart-button').addEventListener('click', start);
 
@@ -29,14 +19,6 @@ function darkMode() {
     let element = document.body;
     element.classList.toggle("dark-mode");
   }
-/*-------------------------------- Functions --------------------------------*/
-
-
-// Some functions you might choose to use:
-
-// Initialization function:
-// Where you set your initial state, setting up
-// what the board will look like upon loading
 
 function start() {
     messages.textContent = 'Welcome!'
@@ -47,9 +29,7 @@ function start() {
 };
     start();
 
-// On-Click function:
-// Set up what happens when one of the elements
-// is clicked
+
 function handleTurn(event) {
     let idx = squares.findIndex((square) => {
       return square === event.target;
@@ -58,21 +38,16 @@ function handleTurn(event) {
         return
     };
     board[idx] = turn;
-    if(turn === 'X') {
-        turn = 'O'
+    if(turn === '🗿') {
+        turn = '🐱‍👓'
     }
-    else {turn = 'X'};
+    else {turn = '🗿'};
 
     win = winnerCheck();
 
     render();
 };
 
-
-// Check winner function:
-// Checks the current state of the board for
-// a winner and changes the state of the winner
-// variable if so
 
 function winnerCheck() {
     let winner = null;
@@ -94,15 +69,6 @@ function winnerCheck() {
 };
 
 
-// Render function:
-// Displays the current state of the board
-// on the page, updating the elements to reflect
-// either X or O depending on whose turn it is
-
-// var confettiElement = document.getElementById('my-canvas');
-// var confettiSettings = { target: confettiElement };
-// var confetti = new ConfettiGenerator(confettiSettings);
-// confetti.render();
 
 function render() {
     board.forEach((mark, index) => {
@@ -110,13 +76,14 @@ function render() {
     });
 
     if ( win === 'Tie' ) {
-        messages.textContent = `Cat's game! Try again!`
-    }
+        board.forEach((mark, index) => {
+            squares[index].textContent = "👽";
+        messages.textContent = `The aliens win. Say goodbye to your crops (and your cattle).`;
+    })}
     else if (win) {
-        messages.textContent = `${win} wins the game!`
-
+        messages.textContent = `${win} wins the game!`;
     }
     else {
-        messages.textContent = `It's ${turn}'s turn!`
+        messages.textContent = `It's ${turn}'s turn!`;
     };
 };
